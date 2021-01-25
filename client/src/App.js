53560bed097686a2c934f28d87801e6b33a7c6d7
keba92 <61150239+keba92@users.noreply.github.com>
2021-01-25 22:01:34 +0200

    v.1.10.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Header from './app/Header/containers/Header';
 import Body from './app/Body/Body';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Spinner } from 'react-bootstrap';
+
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Spinner animation="border" variant="primary" />;
+  }
 
   return (
     <>
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { useHistory } from "react-router-dom";
 import { Auth0Provider } from '@auth0/auth0-react';
 import './i18n'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const history = useHistory();
+
+const onRedirectCallback = (appState) => {
+  history.push(appState?.returnTo || window.location.pathname);
+};
 
 ReactDOM.render(
   <Suspense fallback={(<div>Loading ...</div>)}>
@@ -14,7 +21,8 @@ ReactDOM.render(
     clientId={clientId}
     redirectUri={window.location.origin}
     audience="https://dev-lma8p4gy.eu.auth0.com/api/v2/"
-    scope="read:current_user update:current_user_metadata">
+    scope="read:current_user update:current_user_metadata"
+    onRedirectCallback={onRedirectCallback}>
     <App />
   </Auth0Provider>
   </Suspense>,
